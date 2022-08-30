9d194edbf12873acd6f4eda63b2d10e56910134a
Rhea Epperson <re.epperson2@gmail.com>
2022-08-30 17:44:09 -0400

    JS Intro Excercise

---
+++ app.js
@@ -0,0 +1,24 @@
+const firstName = "Rhea"
+let lastName = "Epperson"
+var age = 25
+
+firstName = "Rhea"
+lastName = "Epperson"
+age = 25
+
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+let price = 19.99
+let isOneSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+let title ="Name of the Wind"
+let author = " Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
